4b8ddb038f97eeeb708f64afe623b427738cde6b
bachnnt1 <ngocbach.nguyen@yahoo.com.vn>
2022-02-18 11:12:00 +0700

    sections about, footer, sticky header

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -2,9 +2,32 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import "./About.scss";
 class About extends Component {
   render() {
-    return <div>About</div>;
+    return (
+      <div className="section-about">
+        <div className="section-about-header">Truyền thông nói về BachNNT</div>
+        <div className="section-about-content">
+          <div className="content-left">
+            <iframe
+              width="100%"
+              height="440px"
+              src="https://www.youtube.com/embed/YMrJ8p4zlOc"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="content-right">
+            <div className="img img1"></div>
+            <div className="img img2"></div>
+            <div className="img img3"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
