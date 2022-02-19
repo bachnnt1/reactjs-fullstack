590f50bb269cd45d5ac6b62490aa9d17f880341c
bachnnt <bachcvb@gmail.com>
2022-02-19 14:50:34 +0700

    Modify header for user manage

--- src/routes/System.js
+++ src/routes/System.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
-import UserManage from "../containers/System/UserManage";
-import ProductManage from "../containers/System/ProductManage";
-import RegisterPackageGroupOrAcc from "../containers/System/RegisterPackageGroupOrAcc";
 import Header from "../containers/Header/Header";
+import UserManage from "../containers/System/UserManage";
+import UserRedux from "../containers/System/UserRedux";
 class System extends Component {
   render() {
     const { systemMenuPath, isLoggedIn } = this.props;
@@ -15,11 +14,7 @@ class System extends Component {
           <div className="system-list">
             <Switch>
               <Route path="/system/user-manage" component={UserManage} />
-              <Route path="/system/product-manage" component={ProductManage} />
-              <Route
-                path="/system/register-package-group-or-account"
-                component={RegisterPackageGroupOrAcc}
-              />
+              <Route path="/system/user-redux" component={UserRedux} />
               <Route
                 component={() => {
                   return <Redirect to={systemMenuPath} />;
