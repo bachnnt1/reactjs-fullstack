7811146186d343649061815007e259af657a6de2
bachnnt <bachcvb@gmail.com>
2022-02-19 14:17:55 +0700

    move Header to System

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,6 @@ import { path } from "../utils";
 
 import Home from "../routes/Home";
 import Login from "./Auth/Login";
-import Header from "./Header/Header";
 import System from "../routes/System";
 
 import { CustomToastCloseButton } from "../components/CustomToast";
@@ -46,7 +45,6 @@ class App extends Component {
         <Router history={history}>
           <div className="main-container">
             <ConfirmModal />
-            {this.props.isLoggedIn && <Header />}
 
             <div className="content-container">
               <CustomScrollbars style={{ height: "100vh", width: "100%" }}>
