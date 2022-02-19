7d9966fe24f529f021d5b6753027aa3182345d1e
bachnnt <bachcvb@gmail.com>
2022-02-19 15:34:58 +0700

    Multilanguage in Redux

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,43 +1,62 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import * as actions from "../../store/actions";
-import Navigator from '../../components/Navigator';
-import { adminMenu } from './menuApp';
-import './Header.scss';
-
+import Navigator from "../../components/Navigator";
+import { adminMenu } from "./menuApp";
+import "./Header.scss";
+import { languages } from "../../utils";
+import { changeLanguageApp } from "../../store/actions";
 class Header extends Component {
+  changeLanguage = (language) => {
+    this.props.changeLanguageAppRedux(language);
+  };
 
-    render() {
-        const { processLogout } = this.props;
+  render() {
+    const { processLogout } = this.props;
+    return (
+      <div className="header-container">
+        {/* thanh navigator */}
+        <div className="header-tabs-container">
+          <Navigator menus={adminMenu} />
+        </div>
 
-        return (
-            <div className="header-container">
-                {/* thanh navigator */}
-                <div className="header-tabs-container">
-                    <Navigator menus={adminMenu} />
-                </div>
-
-                {/* nút logout */}
-                <div className="btn btn-logout" onClick={processLogout}>
-                    <i className="fas fa-sign-out-alt"></i>
-                </div>
-            </div>
-        );
-    }
-
+        <div className="language-container">
+          <div
+            className={
+              this.props.lang === languages.VI ? "flag active" : "flag"
+            }
+          >
+            <span onClick={() => this.changeLanguage(languages.VI)}>VN</span>
+          </div>
+          <div
+            className={
+              this.props.lang === languages.EN ? "flag active" : "flag"
+            }
+          >
+            <span onClick={() => this.changeLanguage(languages.EN)}>EN</span>
+          </div>
+          {/* nút logout */}
+          <div className="btn btn-logout" onClick={processLogout}>
+            <i className="fas fa-sign-out-alt"></i>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        isLoggedIn: state.user.isLoggedIn
-    };
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        processLogout: () => dispatch(actions.processLogout()),
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {
+    processLogout: () => dispatch(actions.processLogout()),
+    changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language)),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
--- src/routes/System.js
+++ src/routes/System.js
@@ -35,7 +35,7 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = () => {
   return {};
 };
 
