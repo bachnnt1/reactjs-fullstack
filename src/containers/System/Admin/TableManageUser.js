1f5bc366290ad60bc6778d5faed883c354a7ed9a
bachnnt <bachcvb@gmail.com>
2022-02-22 13:54:07 +0700

    install markdown editor

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -5,6 +5,23 @@ import {
   fetchAllUsers,
   deleteUserRedux,
 } from "../../../store/actions/adminActions";
+
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// import style manually
+import "react-markdown-editor-lite/lib/index.css";
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+  console.log("handleEditorChange", html, text);
+}
+
 class TableManageUser extends Component {
   constructor(props) {
     super(props);
@@ -59,6 +76,11 @@ class TableManageUser extends Component {
                 })}
             </tbody>
           </table>
+          <MdEditor
+            style={{ height: "500px" }}
+            renderHTML={(text) => mdParser.render(text)}
+            onChange={handleEditorChange}
+          />
         </div>
       </div>
     );
